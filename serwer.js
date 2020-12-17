be0bc0abe980c54cc64d2f9785d9ef43060663bd
Benamin1 <benekbuz@gmail.com>
2020-12-17 23:04:49 +0100

    serwer ready

--- serwer.js
+++ serwer.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
+var PORT = Process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
     res.send("<h1>first app on heroku!</h1>")
